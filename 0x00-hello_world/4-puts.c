7bccd401789715d0c76ab06cc078e0bf01bb462b
Tashenica <tashenicakleintjie@gmail.com>
2023-02-16 07:30:46 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	tabputs("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }	
