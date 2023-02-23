ed35d1e733d6dc1cba93b9a628ae0cf447e2f339
Tashenica <tashenicakleintjie@gmail.com>
2023-02-23 09:51:19 -0800

    numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,8 @@
 
 /**
  * print_most_numbers - Print the numbers since 0 up to 9
- * Description: Prints the number excluding 2 and 4
- * Return: The numbers 0 up to 9
+ * Description: Prints the numbers excluding 2 and 4
+ * Return: The numbers since 0 up to 9
  */
 
 void print_most_numbers(void)
@@ -13,11 +13,13 @@ void print_most_numbers(void)
 	for (; x <= 9; x++)
 	{
 	if (x == 2 || x == 4)
+	{
 	continue;
 	}
-	if else
+	else
 	{
 	_putchar(x + '0');
+	}
 	}
 	_putchar('\n');
 }
